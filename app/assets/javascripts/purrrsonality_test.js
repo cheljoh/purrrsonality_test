222b7e059a94ed7d13d4ef0429faecf78e27ed99
Chelsea Johnson <cheljoh@gmail.com>
2016-04-21 12:22:36 -0600

    Refactor
    Refactored javascript, trouble shooting tests.

--- app/assets/javascripts/purrrsonality_test.js
+++ app/assets/javascripts/purrrsonality_test.js
@@ -1,3 +1,5 @@
+var results;
+
 $(document).ready(function(){
   $("button[name=get-cat]").on("click", getRandomCat);
   $("button[name=get-questions]").on("click", getQuestions);
@@ -25,6 +27,7 @@ function postData(){
     data: all_answers,
     success: function(response){ //will get response back
       console.log(response)//get results
+      $(".questions").hide()
     },
     error: function(){
     console.log("Something went wrong")
@@ -55,20 +58,8 @@ function getQuestions(){
     method: "GET",
     dataType: "json",
     success: function(questions){
-      $(".questions").html()
-      questionOptions()
-      $(questions).each(function(index, test_question){
-        var number1 = (test_question.id + '-1')
-        var number2 = (test_question.id + '-2')
-        var number3 = (test_question.id + '-3')
-        var number4 = (test_question.id + '-4')
-        var number5 = (test_question.id + '-5')
-        var group = (test_question.id)
-      $(".questions")
-        .append("<div class='row'> <div class='col s5 offset-s1'>" + test_question.question + "</div> <div class='col s5 offset-s1'><div class='responses'> <input name='" + group + "' value = '1' type='radio' id='" + number1 + "'/> <label for='" + number1 + "'>1</label> </div> <div class='responses'> <input name='" + group + "' value = '2' type='radio' id='" + number2 + "'/> <label for='" + number2 + "'>2</label> </div> <div class='responses'> <input name='" + group + "' value = '3' type='radio' id='" + number3 + "' /> <label for='" + number3 + "'>3</label> </div> <div class='responses'> <input name='" + group + "' value = '4' type='radio' id='" + number4 + "' /> <label for='" + number4 + "'>4</label> </div> <div class='responses'> <input name='" + group + "' value = '5' type='radio' id='" + number5 + "' /> <label for='" + number5 + "'>5 </label> </div> </div></div>")
-      })
-      $(".questions")
-      .append("<br><div class='center-align'><button class='submit button btn cyan accent-4'>Submit!</button></div><br><br>")
+      renderQuestions(questions)
+      hideButton()
     },
     error: function(){
     console.log("Something went wrong")
@@ -76,7 +67,28 @@ function getQuestions(){
   })
 }
 
+function renderQuestions(questions){
+  $(".questions").html()
+  questionOptions()
+  $(questions).each(function(index, test_question){
+    var number1 = (test_question.id + '-1')
+    var number2 = (test_question.id + '-2')
+    var number3 = (test_question.id + '-3')
+    var number4 = (test_question.id + '-4')
+    var number5 = (test_question.id + '-5')
+    var group = (test_question.id)
+  $(".questions")
+    .append("<div class='row'> <div class='col s5 offset-s1'>" + test_question.question + "</div> <div class='col s5 offset-s1'><div class='responses'> <input name='" + group + "' value = '1' type='radio' id='" + number1 + "'/> <label for='" + number1 + "'>1</label> </div> <div class='responses'> <input name='" + group + "' value = '2' type='radio' id='" + number2 + "'/> <label for='" + number2 + "'>2</label> </div> <div class='responses'> <input name='" + group + "' value = '3' type='radio' id='" + number3 + "' /> <label for='" + number3 + "'>3</label> </div> <div class='responses'> <input name='" + group + "' value = '4' type='radio' id='" + number4 + "' /> <label for='" + number4 + "'>4</label> </div> <div class='responses'> <input name='" + group + "' value = '5' type='radio' id='" + number5 + "' /> <label for='" + number5 + "'>5 </label> </div> </div></div>")
+  })
+  $(".questions")
+  .append("<br><div class='center-align'><button class='submit button btn cyan accent-4'>Submit!</button></div><br><br>")
+}
+
 function questionOptions() {
   $(".questions")
     .append("<div class='center-align'><p> 1 = Inaccurate <span class='tab-space'> 2 = Somewhat Inaccurate </span> <span class='tab-space'> 3 = Neutral </span> <span class='tab-space'> 4 = Somewhat Accurate </span> <span class='tab-space'> 5 = Accurate</p></space></div> <br>")
+}
+
+function hideButton(){
+  $("button[name=get-questions]").hide()
 }
--- spec/features/user_submits_test_spec.rb
+++ spec/features/user_submits_test_spec.rb
@@ -3,15 +3,22 @@ require "rails_helper"
 RSpec.feature "UserSubmitsTestSpec", type: :feature do
 
   scenario "user submits test", js: true do #solved by moving html out of ajax request? when js true is on cant find
+    pending
     visit "/"
     # click_on "Take the Test!"
     # sleep 10
-# save_and_open_page
-    # choose("1-1")
+    choose("1")
+    # find(:radio_button, '1', checked: false)
 
   end
 
 end
+
+#input[group][value]
+
+#capybara execute script, pass jquery do check buttons
+
+#update firefox
 
 
 # As a user
