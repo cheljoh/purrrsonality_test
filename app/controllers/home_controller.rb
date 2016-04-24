26049a4f38a9a1679e61ba30429d4a4a146e5202
Chelsea Johnson <cheljoh@gmail.com>
2016-04-24 17:05:42 -0600

    More troubleshooting
    Trying different configurations

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
 
   def index
-    @cat = Cat.find_by(name: params["cat"]) if params.has_key?("cat")
+    #@cat = Cat.find_by(name: params["cat"]) if params.has_key?("cat")
+
+    if params.has_key?("cat")
+      @cat = Cat.find_by(name: params["cat"])
+    else
+      @cat = Cat.find_by(name: "Colonel Meow")
+    end
+    # @default_cat = params.has_key?("cat")
   end
 end
