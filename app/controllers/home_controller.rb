9437cbd179a32b715b28694e052452af321953df
Chelsea Johnson <cheljoh@gmail.com>
2016-04-24 10:50:34 -0600

    FB doesn't like file path for picture
    So changed it to a URL.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    @picture = "/assets/grumpy_cat.png"
+    @picture = "http://cdn.grumpycats.com/wp-content/uploads/2012/09/GC-Gravatar-copy.png"
     # @picture = "http://cdn.movieweb.com/img.news.tops/NENmsdRXWCz0RR_2_b.jpg"
   end
 end
