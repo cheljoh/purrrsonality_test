ea442ed2bf5847287c92a7edbd70cbf5c724758f
Chelsea Johnson <cheljoh@gmail.com>
2016-04-23 21:46:46 -0600

    Share Button
    Did not work with localhost:3000 as shared page. Works with herokuapp but does not
    show results of the assessment so far.

---
+++ spec/features/user_shares_results_on_facebooks_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.feature "UserSharesResultsonFacebook", type: :feature do
+
+  scenario "user completes test, views results, and shares on facebook", js: true do
+    pending
+    visit "/"
+    click_on "Take the Test!"
+
+    wait_for_ajax
+
+    50.times do |index|
+      question_id = ("#{index + 1}-1")
+      page.execute_script("$('#" + question_id + "').prop('checked', true).trigger('click')")
+    end
+
+    click_on "Submit!"
+
+    wait_for_ajax
+
+    click_link ".fb-share-button"
+  end
+end
