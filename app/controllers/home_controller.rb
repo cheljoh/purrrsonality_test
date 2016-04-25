77a23c89f3dd4acd376f635876352c795ed029ed
Chelsea Johnson <cheljoh@gmail.com>
2016-04-24 19:30:48 -0600

    Revert
    Conditional didn't work.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,8 @@ class HomeController < ApplicationController
   def index
     if params.has_key?("cat")
       @cat = Cat.find_by(name: params["cat"])
-    # else
-    #   @cat = Cat.find_by(name: "Colonel Meow")
+    else
+      @cat = Cat.find_by(name: "Colonel Meow")
     end
   end
 end
